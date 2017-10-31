f3e5a1a218da1fa8946642131b55f724fb328f09
William Jimenez <william@rancher.com>
2017-10-30 19:19:29 -0500

    sleep after initial deploy

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 kubectl  create -f jenkins-svc.yml
 kubectl  create -f jenkins-deployment.yml
+echo "sleeping 60 seconds to wait for pod to start"
+sleep 60
 kubectl exec $(kubectl get pods -o=name | grep jenkins | cut -d "/" -f2-) cat /var/jenkins_home/secrets/initialAdminPassword
